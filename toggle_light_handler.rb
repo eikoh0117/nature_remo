1511f6224ae622fd09f1eab05ac5595f5caf2f1d
Hidemitsu Aoki <olympos0117@gmail.com>
2021-03-11 22:11:59 +0900

    Create handler

---
+++ toggle_light_handler.rb
@@ -0,0 +1,26 @@
+require 'faraday'
+require 'dotenv/load'
+
+def request
+  client = Faraday.new url: ENV['BASE_URL']
+  client.headers['Authorization'] = "Bearer #{ENV['TOKEN']}"
+  params = { button: "next" }
+  client.post do |req|
+    req.url "/1/appliances/#{ENV['APPLIANCE_ID']}/light"
+    req.body = params
+  end
+end
+
+def turn_on
+  1.times { request() }
+end
+
+def turn_off
+  2.times { request() }
+end
+
+def lambda_handler
+  turn_on()
+end
+
+lambda_handler()
